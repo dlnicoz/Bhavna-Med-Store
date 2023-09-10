1e3d8a21fd162a27d77700bd09d39f6fbc26eeba
dlnicoz <92986739+dlnicoz@users.noreply.github.com>
2023-09-10 15:16:47 +0530

    basic setting done and initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './Home'
+import Create from './Create'
+import Update from './Update'
+import Read from './Read'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/create' element={<Create />}></Route>
+        <Route path='/Update/:id' element={<Update />}></Route>
+        <Route path='/Read/:id' element={<Read />}></Route>
+      </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Home() {
+  
+  return (
+    <div className='d-flex flex-column justify-content-center align-items-center bg-light vh-100'>
+      <h1>Bhavna Medical Store</h1>
+      <h3>List of customers</h3>
+      <div className="w-75 rounded bg-white border shadow p-4">
+
+        
+      </div>
+
+    </div>
+  )
+}
+
+export default Home
